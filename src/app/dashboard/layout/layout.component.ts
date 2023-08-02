d74654eacf4014e63b8808f0f99b6172c0821d6c
Milagros <milagrosreynamarchesi@gmail.com>
2023-08-01 23:41:12 -0300

    Organizacion por modulos

---
+++ src/app/dashboard/layout/layout.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.navigate(['/dashboard', 'home'], {});
+  }
+
+  abrirAlumnos(){
+    this.router.navigate(['/dashboard', 'alumnos']);
+  }
+  abrirHome(){
+    this.router.navigate(['/dashboard', 'home'], {});
+  }
+
+  logout(): void {
+    this.router.navigate(['auth', 'login'], {})
+  }
+}
+
