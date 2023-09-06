ddddfea401f6cd0b5a60ca9b1254aa1a6470353a
Milagros <milagrosreynamarchesi@gmail.com>
2023-09-06 01:37:25 -0300

    enrollments desde alumnos y cursos

--- src/app/dashboard/pages/cursos/pages/detalle-curso/detalle-curso.component.ts
+++ src/app/dashboard/pages/cursos/pages/detalle-curso/detalle-curso.component.ts
@@ -1,6 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CourseInterface } from '../../models/curso.interface'; 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectListEnrollments } from 'src/app/store/inscripciones/inscripciones.selectors';
+import { InscripcionesInterface } from '../../../inscripciones/models/inscripciones.interface';
+import { inscripcionesDelete, inscripcionesListLoad, inscripcionesListStore } from 'src/app/store/inscripciones/inscripciones.actions';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-detalle-curso',
@@ -9,5 +15,20 @@ import { CourseInterface } from '../../models/curso.interface';
 })
 
 export class DetalleCursoComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: CourseInterface) {}
+  enrollments$: Observable<any> = new Observable();
+  constructor(private store:Store<any>, @Inject(MAT_DIALOG_DATA) public data: CourseInterface) {
+  }
+
+  ngOnInit() {
+    this.enrollments$ = this.store.select(selectListEnrollments);
+    this.store.dispatch(inscripcionesListLoad())
+  }
+
+  eliminar(enrollment: InscripcionesInterface | null){
+    if (enrollment) {
+      this.store.dispatch(inscripcionesDelete({ enrollment }));
+      this.store.dispatch(inscripcionesListLoad())
+    }
+  }
 }
+
